9f8f95fd1a47616da535e9b50302ea999e30d9ac
M-Piper <margaret.f.piper@gmail.com>
2023-10-31 17:41:10 -0400

    Oct31-23
    Halfway through tutorial three
    Added new background

--- Assets/C#_Scripts/playerInput.cs
+++ Assets/C#_Scripts/playerInput.cs
@@ -13,6 +13,52 @@ public class playerInput : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        GetMouseClick();
+    }
+
+    void GetMouseClick()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 mousePosition = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, -10));
+            RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
+
+            if (hit)
+            {
+                if (hit.collider.CompareTag("Deck"))
+                {
+                    Deck();
+                }
+                else if (hit.collider.CompareTag("Card"))
+                {
+                    Card();
+                }
+                else if (hit.collider.CompareTag("Foundation"))
+                {
+                    Foundation();
+                }
+                else if (hit.collider.CompareTag("Tableau"))
+                {
+                    Tableau();
+                }
+            }
+        }
+    }
+
+    void Deck()
+    {
+        print("clicked on Deck");
+    }
+    void Card()
+    {
+        print("clicked on Card");
+    }
+    void Foundation()
+    {
+        print("clicked on Foundation");
+    }
+    void Tableau()
+    {
+        print("clicked on Tableau");
     }
 }
